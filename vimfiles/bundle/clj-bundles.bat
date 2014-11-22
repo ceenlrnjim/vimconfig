52c643f35c222bf259f76be57851ae1acd9b5627
James Kirkwood <kirkwojl@hotmail.com>
2014-11-22 13:07:54 -0500

    added script to pull pathogen bundles for clojure dev

---
+++ vimfiles/bundle/clj-bundles.bat
@@ -0,0 +1,5 @@
+git clone git://github.com/tpope/vim-classpath.git
+git clone git://github.com/tpope/vim-dispatch.git
+git clone git://github.com/tpope/vim-fireplace.git
+git clone git://github.com/tpope/vim-leiningen.git
+git clone git://github.com/tpope/vim-projectionist.git
