34ca1f49b77ad505ba717c745cd7fe6dc2127c45
James Kirkwood <kirkwojl@hotmail.com>
2014-11-22 20:16:27 -0500

    added scratch.vim

--- vimfiles/bundle/clj-bundles.bat
+++ vimfiles/bundle/clj-bundles.bat
@@ -4,3 +4,4 @@ git clone git://github.com/tpope/vim-dispatch.git
 git clone git://github.com/tpope/vim-fireplace.git
 git clone git://github.com/tpope/vim-leiningen.git
 git clone git://github.com/tpope/vim-projectionist.git
+git clone https://github.com/mtth/scratch.vim
