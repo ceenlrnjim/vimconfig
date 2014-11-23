a35f22671d39554053bde8f42362468d8e9be1df
James Kirkwood <kirkwojl@hotmail.com>
2014-11-22 20:01:18 -0500

    added vim-clojure-static

--- vimfiles/bundle/clj-bundles.bat
+++ vimfiles/bundle/clj-bundles.bat
@@ -1,3 +1,4 @@
+git clone https://github.com/guns/vim-clojure-static.git
 git clone git://github.com/tpope/vim-classpath.git
 git clone git://github.com/tpope/vim-dispatch.git
 git clone git://github.com/tpope/vim-fireplace.git
